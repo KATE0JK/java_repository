b8be2025832d0edbad606ba1c62a9c5d7eddfd0f
BY-MINSK\KatyaTratsevskaya <Kate0jk@gmail.com>
2018-01-24 16:13:40 +0300

    no message

--- sendbox/src/main/java/MyFirstProgram.java
+++ sendbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,19 @@
 public class MyFirstProgram {
 
    public static void main(String[] args) {
-      System.out.println("Hello, world!");
+      double l = 5;
+      System.out.println ("S квадрат со стороной " + l + " = " + area (l));
+      double a = 2;
+      double b = 5;
+      System.out.println("S квадрат со стороной " + a + " и " + b + " = " + area (a,b));
    }
+
+   public static double area (double a,double b) {
+      return a*b;
+   }
+
+   public static double area (double l) {
+      return l*l;
+   }
+
 }

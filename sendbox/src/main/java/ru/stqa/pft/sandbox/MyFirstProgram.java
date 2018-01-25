6c745be36efc35237aeaad9846a287362365bacb
BY-MINSK\KatyaTratsevskaya <Kate0jk@gmail.com>
2018-01-25 12:17:03 +0300

    Methods and functions

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,19 +6,19 @@ public class MyFirstProgram {
 
    public static void main(String[] args) {
       Square s = new Square (5);
-      System.out.println ("S квадрат со стороной " + s.l + " = " + area (s));
+      System.out.println ("S квадрат со стороной " + s.l + " = " + s.area());
 
       Rectangle r = new Rectangle(2,5);
-      System.out.println("S прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area (r));
+      System.out.println("S прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
    }
 
-   public static double area (Square s) {
-      return s.l*s.l;
-   }
+//   public static double area (Square s) {
+//      return s.l*s.l;
+//   }
 
-   public static double area (Rectangle r) {
-
-      return r.a * r.b;
-   }
+//   public static double area (Rectangle r) {
+//
+//      return r.a * r.b;
+//   }
 
 }
